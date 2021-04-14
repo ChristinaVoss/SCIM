bcc1671ab64d79fd20a3d07de2aa1da746e4f18b
Christina Voss <chustvedt@gmail.com>
2021-04-14 13:43:19 +0100

    Update init file with initial core functionality
    close #6

---
+++ schoolclubinfomanager/__init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'supersecret' #to allow us to use forms, not safe for deployment
+
+##########################################
+############ DATABASE SETUP ##############
+##########################################
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+##########################################
+######### LOGIN CONFIGURATION ############
+##########################################
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
