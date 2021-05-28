c1b19efe8590b6d86f97a5fc8ec0d5cdb0d9869e
Christina Voss <chustvedt@gmail.com>
2021-05-28 14:11:47 +0100

    Update clubs.views.py and related files to extract data from Create Club and update database.
    Closes #59

--- schoolclubinfomanager/clubs/picture_handler.py
+++ schoolclubinfomanager/clubs/picture_handler.py
@@ -7,7 +7,7 @@ def add_photo(pic_upload, club_name):
     #grab extension type, such as png or jpg:
     ext_type = filename.split('.')[-1]
     #set image filename as <schoolname-logo.jpg> (or png, or svg):
-    storage_filename = str(club_name)+ext_type
+    storage_filename = str(club_name)+'.'+ext_type
     filepath = os.path.join(current_app.root_path, "static/img/club", storage_filename)
 
     #open image supplied by user and save it
